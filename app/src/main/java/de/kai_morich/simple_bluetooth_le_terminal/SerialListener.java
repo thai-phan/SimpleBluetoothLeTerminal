5f5bd35fbed9298ca7dc3a8f438977befac69d9e
thai-phan <thaiph@gaion.kr>
2023-10-17 11:33:12 +0900

    refactor code

--- app/src/main/java/de/kai_morich/simple_bluetooth_le_terminal/SerialListener.java
+++ app/src/main/java/de/kai_morich/simple_bluetooth_le_terminal/SerialListener.java
@@ -3,9 +3,13 @@ package de.kai_morich.simple_bluetooth_le_terminal;
 import java.util.ArrayDeque;
 
 interface SerialListener {
-    void onSerialConnect      ();
-    void onSerialConnectError (Exception e);
-    void onSerialRead         (byte[] data);                // socket -> service
-    void onSerialRead         (ArrayDeque<byte[]> datas);   // service -> UI thread
-    void onSerialIoError      (Exception e);
+  void onSerialConnect();
+
+  void onSerialConnectError(Exception e);
+
+  void onSerialRead(byte[] data);                // socket -> service
+
+  void onSerialRead(ArrayDeque<byte[]> datas);   // service -> UI thread
+
+  void onSerialIoError(Exception e);
 }
